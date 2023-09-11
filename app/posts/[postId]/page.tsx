7fae76ef1951b3673cd07ad0e34bdb565762b09e
smillanpardo <smillan_ext@emergya.com>
2023-09-10 19:50:41 -0500

    ajuste autor

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -35,9 +35,9 @@ return (
     <div className="max-w-2xl mx-auto p-4">
         <Link href="/" className="text-sm text-gray-800"> {"< Home"} </Link><br/>
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
-      <p className="text-lg text-gray-700 mb-2">Author: <Link href={`/users/${author.id}`}>{author.name}</Link></p>
+      <p className="text-lg text-gray-700 mb-2">Autor: <Link href={`/users/${author.id}`}>{author.name}</Link></p>
       <p className="text-lg text-gray-800 leading-relaxed mb-4">{post.body}</p>
-      <h2 className="text-xl font-semibold mb-2">Comments:</h2>
+      <h2 className="text-xl font-semibold mb-2">Comentarios:</h2>
       {comments.map((comment :commentInterface) => (
         <div key={comment.id} className="bg-white rounded-lg shadow-md p-4 mb-4">
           <h3 className="text-lg font-medium" >{comment.email}</h3>
